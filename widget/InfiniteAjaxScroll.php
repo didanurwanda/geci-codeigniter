9a69bf4cdff3ed5f041c6f64fdba791583e44e7f
didanurwanda <dida_n@ymail.com>
2013-07-10 22:40:37 +0700

    GeCi 1.0

--- widget/InfiniteAjaxScroll.php
+++ widget/InfiniteAjaxScroll.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * GeCi 
+ *
+ * Widget dan Library Open Source untuk PHP Framework CodeIgniter
+ *
+ * @peckage			GeCi
+ * @author			Dida Nurwanda
+ * @email			didanurwanda@gmail.com
+ * @blog			didanurwanda.blogspot.com
+ * @copyright		Copyright (c) 2013
+ * @license			http://geci.sourceforge.net/license.html
+ * @link			http://geci.sourceforge.net
+ * @sience			Version 1.0
+ */
+ 
+/**
+ * @class			InfiniteAjaxScroll
+ * @peckage			Widget
+ * @subpeckage		Widget
+ * @category		Widget
+ * @author			Dida Nurwanda
+ */
+
 namespace GeCi\widget;
 
 use \GeCi\component\Html;
--- widget/jui/Spinner.php
+++ widget/jui/Spinner.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * GeCi 
+ *
+ * Widget dan Library Open Source untuk PHP Framework CodeIgniter
+ *
+ * @peckage			GeCi
+ * @author			Dida Nurwanda
+ * @email			didanurwanda@gmail.com
+ * @blog			didanurwanda.blogspot.com
+ * @copyright		Copyright (c) 2013
+ * @license			http://geci.sourceforge.net/license.html
+ * @link			http://geci.sourceforge.net
+ * @sience			Version 1.0
+ */
+ 
+/**
+ * @class			Spinner
+ * @peckage			Jui Widget
+ * @subpeckage		Jui Widget
+ * @category		Widget
+ * @author			Dida Nurwanda
+ */
+
 namespace GeCi\widget\jui;
 
 use \GeCi\component\Html;
