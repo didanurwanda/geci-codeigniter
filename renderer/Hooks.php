259fd8b67db6682da0a70567b163d1cc6a8951e6
didanurwanda <dida_n@ymail.com>
2013-07-10 22:43:57 +0700

    GeCi 1.0

--- renderer/Hooks.php
+++ renderer/Hooks.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * GeCi 
+ *
+ * Widget dan Library Open Source untuk PHP Framework CodeIgniter
+ *
+ * @peckage			GeCi
+ * @author			Dida Nurwanda
+ * @email			didanurwanda@gmail.com
+ * @blog			didanurwanda.blogspot.com
+ * @copyright		Copyright (c) 2013
+ * @license			http://geci.sourceforge.net/license.html
+ * @link			http://geci.sourceforge.net
+ * @sience			Version 1.0
+ */
+ 
+/**
+ * @class			Hooks
+ * @peckage			Renderer
+ * @subpeckage		Component
+ * @category		Component
+ * @author			Dida Nurwanda
+ */
+
 namespace GeCi\renderer;
 
 use \GeCi\component\Component;
