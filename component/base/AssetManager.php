1943a96989febe6c37c74c038329a068f29ab78d
didanurwanda <dida_n@ymail.com>
2013-07-10 22:42:26 +0700

    GeCi 1.0

--- component/base/AssetManager.php
+++ component/base/AssetManager.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * GeCi 
+ *
+ * Widget dan Library Open Source untuk PHP Framework CodeIgniter
+ *
+ * @peckage			GeCi
+ * @author			Dida Nurwanda
+ * @email			didanurwanda@gmail.com
+ * @blog			didanurwanda.blogspot.com
+ * @copyright		Copyright (c) 2013
+ * @license			http://geci.sourceforge.net/license.html
+ * @link			http://geci.sourceforge.net
+ * @sience			Version 1.0
+ */
+ 
+/**
+ * @class			AssetManager
+ * @peckage			Component
+ * @subpeckage		Component
+ * @category		Component
+ * @author			Dida Nurwanda
+ */
+
 namespace GeCi\component\base;
 
 class AssetManager extends \GeCi\component\Component
